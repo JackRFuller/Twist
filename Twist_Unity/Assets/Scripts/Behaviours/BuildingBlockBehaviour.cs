ab40374f2a7c4367ceae5db9558289a645139561
JackRFuller <jackrfuller@outlook.com>
2016-03-30 22:19:40 +0100

    Basic Object Spawning
    Cubes now spawn and move down

---
+++ Twist_Unity/Assets/Scripts/Behaviours/BuildingBlockBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingBlockBehaviour : MonoBehaviour {
+
+    [HideInInspector] public Material currentColour;
+
+    [Header("Movement Attributes")]
+    [SerializeField] private float speed;
+    private CharacterController cc;
+    private bool inPlace;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (!inPlace)
+            Move();
+    }
+
+    void Move()
+    {
+        Vector3 _movementDirection = new Vector3(0, 0, 0);
+        _movementDirection = transform.TransformDirection(_movementDirection);
+
+        _movementDirection.y -= speed * Time.deltaTime;
+        cc.Move(_movementDirection * Time.deltaTime);
+
+        DetectGround();
+    }
+
+    void DetectGround()
+    {
+        
+        Vector3 down = transform.TransformDirection(Vector3.down);
+        Ray ray = new Ray(transform.position, down);
+        //Debug.DrawRay(ray.origin, ray.direction, Color.green);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 10f))
+        {
+            float _dist = Vector3.Distance(transform.position, hit.transform.position);
+            if(_dist < 1f)
+            {
+                inPlace = true;                
+                transform.parent = hit.transform.parent.root;
+                //transform.localPosition = new Vector3(hit.transform.position.x,
+                //                                      hit.transform.position.y + 1f,
+                //                                      hit.transform.position.z);
+
+            }
+        }
+
+    }
+}
---
+++ Twist_Unity/Assets/Scripts/Managers/WaveManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaveManager : MonoBehaviour {
+
+    [Header("Object Spawning")]
+    [SerializeField] private GameObject block;
+    [SerializeField] private Transform blockTidy;
+    [SerializeField] private int numberOfBlocksToSpawn;
+    private List<GameObject> spawnedBuildingBlocks;
+
+    [Header("Spawn Points")]
+    [SerializeField] private Transform[] spawnPoints = new Transform[4]; 
+    
+    [Header("Colours")]
+    [SerializeField] private Material[] blockColours = new Material[4]; 
+
+    void Start()
+    {
+        SpawnInBlocks();
+    }
+
+    void SpawnInBlocks()
+    {
+        spawnedBuildingBlocks = new List<GameObject>();
+
+        for(int i = 0; i < numberOfBlocksToSpawn; i++)
+        {
+            GameObject _buildingBlock = Instantiate(block,transform.position,block.transform.rotation) as GameObject;
+            _buildingBlock.transform.parent = blockTidy;
+            spawnedBuildingBlocks.Add(_buildingBlock);
+            _buildingBlock.SetActive(false);
+        }
+
+        InvokeRepeating("SpawnCube", 1.0f, 2.0f);
+    }
+
+    void SpawnCube()
+    {
+        GameObject _buildingBlock = null;
+
+        for(int i = 0; i < spawnedBuildingBlocks.Count; i++)
+        {
+            if (!spawnedBuildingBlocks[i].activeInHierarchy)
+            {
+                _buildingBlock = spawnedBuildingBlocks[i];
+            }
+        }
+
+        if (_buildingBlock == null)
+        {
+            _buildingBlock = Instantiate(block, transform.position, block.transform.rotation) as GameObject;
+            _buildingBlock.SetActive(false);
+            _buildingBlock.transform.parent = blockTidy;
+            spawnedBuildingBlocks.Add(_buildingBlock);
+        }
+
+        //ChooseColour
+        int _colour = Random.Range(0, blockColours.Length);
+        _buildingBlock.GetComponent<MeshRenderer>().material = blockColours[_colour];
+        _buildingBlock.GetComponent<BuildingBlockBehaviour>().currentColour = blockColours[_colour];
+
+        //Place Random Block
+        int _spawnPos = Random.Range(0, spawnPoints.Length);
+        _buildingBlock.transform.position = spawnPoints[_spawnPos].position;
+        _buildingBlock.SetActive(true);
+    }
+}
